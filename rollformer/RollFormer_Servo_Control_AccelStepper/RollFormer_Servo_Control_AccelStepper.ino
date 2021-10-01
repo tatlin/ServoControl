db65e2585a3bb416789d16b0665fe412cf023612
Matt Jezyk <matt.jezyk@gmail.com>
2021-10-01 11:26:50 -0400

    works with hardcoded vals and now LED blink

--- rollformer/RollFormer_Servo_Control_AccelStepper/RollFormer_Servo_Control_AccelStepper.ino
+++ rollformer/RollFormer_Servo_Control_AccelStepper/RollFormer_Servo_Control_AccelStepper.ino
@@ -6,27 +6,33 @@ AccelStepper stepper2Lower(AccelStepper::DRIVER,7,8);
 AccelStepper mainDriveServo(AccelStepper::DRIVER,9,10);
 
 //stepper motor constants
-int stepperMotorMaxSpeed = 10000; 
-int motorAccel = 10000; 
+int stepperMotorMaxSpeed = 2000; 
+int motorAccel = 1000; 
 
 //init stepper variables
 int speedStepper = 0;
 int speedVal = 0;
 float stepper1UpperSpeedTweak = 0;
 float stepper2LowerPositionTweak = 0;
- 
+int finalSpeed = 0;
+int finalAccel = 0;
+
+char serialState = "";
+
 void setup() {
   Serial.begin(9600);             // start serial port
   //myServo.attach(servoPin);       // declare to which pin is the servo connected
-  step2.setMaxSpeed (stepperMotorMaxSpeed);  
-  step2.setSpeed(stepperMotorMaxSpeed/10);
-  step2.setAcceleration(motorAccel);
+  mainDriveServo.setMaxSpeed(stepperMotorMaxSpeed);
+  stepper1Upper.setMaxSpeed(stepperMotorMaxSpeed);
+  stepper2Lower.setMaxSpeed(-stepperMotorMaxSpeed);
 }
  
 void loop() {
   while(Serial.available()==0){}; // wait until information is received from the serial port
-    speedStepper = Serial.read(); // read the speed from the OSC control and server
-    Serial.println("speedStepper: " + String(speedStepper));
+    //serialState = Serial.read(); ; // read the speed from the OSC control and server
+    speedStepper = 1000;
+    stepper1UpperSpeedTweak = 0;
+    //Serial.println("speedStepper: " + String(speedStepper));
     finalSpeed = int(abs(speedStepper + stepper1UpperSpeedTweak));
     finalAccel = motorAccel;
     mainDriveServo.setSpeed(speedStepper);
